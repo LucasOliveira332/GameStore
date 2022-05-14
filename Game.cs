052258d86e473da155829c4932ce6d9575d0cf76
Lucas Oliveira <lucas.g.oliveira1@hotmail.com>
2022-05-14 16:16:37 -0300

    Game class finish

---
+++ Game.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesStore
+{
+    public enum Genre { FPS, Battle_Royale, First_Person_Adventure, PVP, MOBA, RPG, MMORPG, All }
+    public enum Plataform { PC, Playstation_4, Playstation_5, Xbox_360, Xbox_One, Android, All }
+    public class Game
+    {
+        private int _id;
+        private string _name;
+        private string _description;
+        public Genre Gen { get; set; }
+        public Plataform Plat { get; set; }
+
+        public Game(int id, string name, Genre gen, Plataform plat, string description)
+        {
+            Id = id;
+            Name = name;
+            Gen = gen;
+            Plat = plat;
+            Description = description;
+        }
+        public Game()
+        {
+            Id = 1;
+            Name = "";
+            Description = "";
+            Gen = Genre.All;
+            Plat = Plataform.All;
+        }
+
+        
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                if (value > 0) _id = value;
+                else throw new Exception("Value invalid");
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value.ToUpper(); }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value.ToUpper(); }
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Id},{Name},{Description}, {Gen}, {Plat}";
+        }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,16 @@ namespace GamesStore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+
+            Game g1 = new Game();
+            g1.Id = 1;
+            g1.Name = "Legue of legends";
+            g1.Description = "Play vs 5 outhers player";
+            g1.Gen = Genre.MOBA;
+            g1.Plat = Plataform.PC;
+
+            Console.WriteLine(g1);
+
         }
     }
 }
